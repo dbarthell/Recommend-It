31ae672ab53a7c6461c19e69ef57f32c7ac42deb
Joe Booth <joseph.booth3@gmail.com>
2018-06-25 11:03:20 -0500

    fixing recommendation model

--- models/recommendation.js
+++ models/recommendation.js
@@ -1,31 +1,32 @@
-// Dependencies
-// =============================================================
+module.exports = function (sequelize, DataTypes) {
 
-// Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
 
-// Creates a "Recommendation" model that matches up with DB
-var Recommendation = sequelize.define("recommendation", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  author: {
-    type: Sequelize.STRING
-  },
-  category: {
-    type: Sequelize.ENUM('books', 'movies', 'shows', 'songs', 'restaurants'),
-  },
-  title: {
-    type: Sequelize.STRING
-  },
-  post: {
-    type: Sequelize.TEXT
-  },
-  image_url: {
-    type: Sequelize.STRING
-  }
-});
+  var Recommendation = sequelize.define("Recommendation", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category: {
+      type: DataTypes.ENUM('books', 'movies', 'shows', 'songs', 'restaurants'),
+      allowNull: false
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    post: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    image_url: {
+      type: Sequelize.STRING
+    }
+  });
+  return Recommendation;
+
+};
