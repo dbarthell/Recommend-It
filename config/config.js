f4ad220a6f2bb5c06307a9122feda5692b027dd8
Nerita Sung <neritasung@gmail.com>
2018-06-25 21:29:15 -0500

    adds config.js file for db password and username

---
+++ config/config.js
@@ -0,0 +1,25 @@
+module.exports = 
+{
+  "development": {
+    "username": process.env.db_username,
+    "password": process.env.db_password,
+    "database": "recommendations_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+
