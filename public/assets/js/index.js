13ea430218d4025d7e74b2ecce750a933e5627f5
Dan Barthell <dbarthell@Dans-MacBook-Pro.local>
2018-07-01 13:17:30 -0500

    fixed route on delete event

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
         url: "/api/posts/" + id
       })
         .then(function() {
-          getPosts(postCategorySelect.val());
+          getPosts();
         });
     }
   
